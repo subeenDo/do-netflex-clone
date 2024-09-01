d2861af78d8219608a27e762d31dcd2cfe778074
subeen <soobin2318@gmail.com>
2024-09-01 23:55:48 +0900

    6-7

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import Banner from './components/Banner/Banner'
+
+//1. 베너 = populer영화를 들고와서 첫번째 아이템을 보여주자
+//2. populer movie
+//3. top rated movie
+//4. upcomming movie
 
 const Homepage = () => {
   return (
-    <div>Homepage</div>
+    <div>
+      <Banner/>
+
+    </div>
   )
 }
 
---
+++ src/pages/Homepage/components/Banner/Banner.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { usePopulerMoviesQery } from '../../../../hooks/usePopulerMovies'
+import Alert from 'react-bootstrap/Alert';
+import "./Banner.css"
+
+const Banner = () => {
+    const {data, isLoading, isError, error}= usePopulerMoviesQery()
+    // console.log("ddd", data);
+    if (isLoading) {
+        return (
+            <div className="d-flex justify-content-center align-items-center" style={{ height: '100vh' }}>
+                <div className="spinner-border text-primary" role="status">
+                    <span className="sr-only">Loading...</span>
+                </div>
+            </div>
+        );
+    }
+
+    if (isError) {
+        return (
+            <Alert variant='danger'>{error.message}</Alert>
+        );
+    }
+
+  return (
+    <div style={{
+        backgroundImage:"url("+`https://media.themoviedb.org/t/p/w533_and_h300_bestv2${data.results[0].poster_path}`+")",
+        }}
+        className='banner'
+    >
+        <div className='text-white banner-text-area'>
+            <h1>{data?.results[0].title}</h1>
+            <p>{data?.results[0].overview}</p>
+        </div>
+    </div>
+  )
+}
+
+export default Banner
